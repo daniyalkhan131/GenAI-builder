908c0f72e8507b19fe281e91414363e4db3d4f3e
daniyalkhan131 <daniyalkhan131@gmail.com>
2025-10-23 12:59:47 +0530

    core concepts of transformer block completed

--- llm_from_scratch/utils.py
+++ llm_from_scratch/utils.py
@@ -96,3 +96,13 @@ class CausalAttention(nn.Module):
 
         context_vec = attn_weights @ values
         return context_vec
+    
+class GELU(nn.Module):
+    def __init__(self):
+        super().__init__()
+
+    def forward(self, x):
+        return 0.5 * x * (1 + torch.tanh(
+            torch.sqrt(torch.tensor(2.0 / torch.pi)) * 
+            (x + 0.044715 * torch.pow(x, 3))
+        ))
